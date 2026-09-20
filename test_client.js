69803b0b2d040ebb056d9fb256e20b7c404b4c0c
Hi <hi@MacBook-Pro-cua-Hi.local>
2026-09-20 10:21:11 +0700

    Dọn dẹp file test

--- test_client.js
+++
@@ -1,2 +0,0 @@
-const io = require('socket.io-client');
-const socket = io('http://localhost:3000'); // Wait, the server is running on localhost? No, the user runs it on Render.
--- test_server.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('server/server.js', 'utf8');
-code = code.replace(/const \{ name, selectedTableIds, settings, starting_stack, buyin_fee \} = data;/, "const { name, selectedTableIds, settings, starting_stack, buyin_fee } = data;\nconsole.log('CREATE_TOUR DATA:', data);");
-fs.writeFileSync('server/server.js', code);
